23aee6d0e9be5a5ce42f12437bb1851e379ecd7e
tao.pei <tao.pei@ck-telecom.com>
2017-05-23 21:00:50 +0800

    rewrite server

--- server/server.c
+++ server/server.c
@@ -10,97 +10,179 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <asm/errno.h>
+#include <errno.h>
 
 #define DEFAULT_PORT 8009
-#define PENDING_MAX 8
+#define PENDING_MAX 5
 
-#define BUFFER_SIZE 40
-#define MAX_EVENTS 10
+#define BUFFER_SIZE 8
+#define MAX_EVENT_NUMBER 10
 
+#define SUCCESS (0)
+
+#define FAILURE (-1)
+
+
+int setnonblocking(int fd) {
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+void addfd(int epoll_fd, int fd, bool enable_et) {
+    struct epoll_event ev;
+    ev.data.fd = fd;
+    ev.events = EPOLLIN;
+    if (enable_et) {
+        ev.events |= EPOLLET;
+    }
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev) == -1) {
+        perror("function addfd:EPOLL_CTL_ADD failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void removefd(int epoll_fd, int fd) {
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL) == -1) {
+        perror("function addfd:EPOLL_CTL_DEL failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void level_triggered_deal(struct epoll_event *events, int num, int epoll_fd, int listen_fd) {
+    char buf[BUFFER_SIZE];
+    int i = 0;
+    for (i = 0; i < num; i++) {
+        int socketfd = events[i].data.fd;
+        if (socketfd == listen_fd) {
+            struct sockaddr_in client_addr;
+            socklen_t client_addr_len = sizeof(client_addr);
+
+            int client_fd = accept(listen_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+
+            addfd(epoll_fd, client_fd, false);
+            setnonblocking(client_fd);
+
+            printf("accept client %s\n", inet_ntoa(client_addr.sin_addr));
+        } else if (events[i].events & EPOLLIN) {
+            printf("event trigger once \n");
+            memset(buf, '\0', BUFFER_SIZE);
+            ssize_t len = recv(socketfd, buf, BUFFER_SIZE - 1, 0);
+            printf("len =  %ld\n", len);
+            if (len <= 0) {
+                removefd(epoll_fd, socketfd);
+                close(socketfd);
+                continue;
+            }
+            printf("receive from client:%s\n", buf);
+            send(socketfd, "I have received your message.", 30, 0);
+        } else {
+            printf("Some unexpected happened!\n");
+        }
+    }
+}
+
+
+void edge_triggered_deal(struct epoll_event *events, int num, int epoll_fd, int listen_fd) {
+    char buf[BUFFER_SIZE];
+    int i = 0;
+    for (i = 0; i < num; i++) {
+        int socketfd = events[i].data.fd;
+        if (socketfd == listen_fd) {
+            struct sockaddr_in client_addr;
+            socklen_t client_addr_len = sizeof(client_addr);
+
+            int client_fd = accept(listen_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+
+            addfd(epoll_fd, client_fd, true);
+
+            printf("accept client %s\n", inet_ntoa(client_addr.sin_addr));
+        } else if (events[i].events & EPOLLIN) {
+            printf("event trigger once \n");
+            while (1) {
+                memset(buf, '\0', BUFFER_SIZE);
+                ssize_t len = recv(socketfd, buf, BUFFER_SIZE - 1, 0);
+                printf("len =  %ld\n", len);
+                if (len < 0) {
+                    if ((EAGAIN == errno) || (EWOULDBLOCK == errno)) {
+                        printf("read later\n");
+                        break;
+                    }
+                    removefd(epoll_fd, socketfd);
+                    close(socketfd);
+                    break;
+                } else if (0 == len) {
+                    removefd(epoll_fd, socketfd);
+                    close(socketfd);
+                    break;
+                } else {
+                    printf("receive from client:%s\n", buf);
+                }
+                send(socketfd, "I have received your message.", 30, 0);
+            }
+        } else {
+            printf("Some unexpected happened!\n");
+        }
+    }
+}
 
 int start_server() {
     int epoll_fd;
 
-    int remote_fd;
-    struct sockaddr_in remote_addr;
-
     int server_socket_fd;
     struct sockaddr_in server_addr;
 
-    socklen_t sockaddr_len = sizeof(struct sockaddr);
-
-
     if ((server_socket_fd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
+
+    int reuse = 1;
+    setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
 
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(DEFAULT_PORT);
 
-    if (bind(server_socket_fd, (struct sockaddr *) &server_addr, sockaddr_len) < 0) {
+    if (bind(server_socket_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
         perror("bind fail");
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
 
     if (listen(server_socket_fd, PENDING_MAX) == -1) {
         perror("listen fail");
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
 
-    epoll_fd = epoll_create(MAX_EVENTS);
+    epoll_fd = epoll_create(MAX_EVENT_NUMBER);
     if (epoll_fd == -1) {
         perror("epoll_create failed");
-        exit(EXIT_FAILURE);
+        return FAILURE;
     }
 
+    addfd(epoll_fd, server_socket_fd, true);
+    setnonblocking(server_socket_fd);
 
-    struct epoll_event events[MAX_EVENTS];
-
-    struct epoll_event ev;
-    ev.events = EPOLLIN;
-    ev.data.fd = server_socket_fd;
-
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_socket_fd, &ev) == -1) {
-        perror("epoll_ctl:server_socket_fd register failed");
-        exit(EXIT_FAILURE);
-    }
-
+    struct epoll_event events[MAX_EVENT_NUMBER];
     while (1) {
-        int num = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        int num = epoll_wait(epoll_fd, events, MAX_EVENT_NUMBER, -1);
         if (num == -1) {
             perror("start epoll_wait failed");
-            exit(EXIT_FAILURE);
+            return FAILURE;
         }
-
-        for (int i = 0; i < num; i++) {
-            if (events[i].data.fd == server_socket_fd) {
-
-                if ((remote_fd = accept(server_socket_fd, (struct sockaddr *) &remote_addr, &sockaddr_len)) < 0) {
-                    perror("accept remote_fd failed");
-                    exit(EXIT_FAILURE);
-                }
-
-                ev.events = EPOLLIN;
-                ev.data.fd = remote_fd;
-                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, remote_fd, &ev) == -1) {
-                    perror("epoll_ctl:epoll_fd register failed");
-                    exit(EXIT_FAILURE);
-                }
-                printf("accept client %s/n", inet_ntoa(remote_addr.sin_addr));
-            } else {
-                char buf[BUFFER_SIZE];
-                int len = recv(remote_fd, buf, BUFFER_SIZE, 0);
-                if (len < 0) {
-                    perror("receive from client failed");
-                    exit(EXIT_FAILURE);
-                }
-                printf("receive from client:%s", buf);
-                send(remote_fd, "I have received your message.", 30, 0);
-            }
-        }
+        printf("++++++++++num = %d\n", num);
+        //edge_triggered_deal(events, num, epoll_fd, server_socket_fd);
+        level_triggered_deal(events, num, epoll_fd, server_socket_fd);
     }
+
+    close(server_socket_fd);
+    return SUCCESS;
+
 
 }
